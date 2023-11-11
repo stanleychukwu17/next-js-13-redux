0f8d44f0f8bcadc0089f565841ad83b3b17f1b2a
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-11-11 20:15:21 +0100

    final changes added

--- app/redux/reduxStore.ts
+++ app/redux/reduxStore.ts
@@ -1,5 +1,5 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import userSlice from './userSlice';
+import userSlice from './features/userSlice';
 
 const rootReducer = combineReducers({
     user: userSlice,
